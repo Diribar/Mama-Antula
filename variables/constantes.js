119fe4a5b7608e478b1af939c13652a5d7426dff
Diego Iribarren <76135160+Diribar@users.noreply.github.com>
2025-10-15 15:55:50 -0300

    Avance del proyecto

--- variables/constantes.js
+++ variables/constantes.js
@@ -1,5 +1,12 @@
 "use strict";
+// Variables
+const unaHora = 60 * 60 * 1000;
+const unDia = unaHora * 24;
+const unaSemana = unDia * 7;
+const unMes = unDia * 30;
+const unAno = unDia * 365;
 
 module.exports = {
 	logoInstitucional: "/imgsEstables/Varios/Institucional.jpg",
+	...{unaHora, unDia, unaSemana, unMes, unAno},
 };
