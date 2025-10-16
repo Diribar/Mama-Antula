b419be070bd4084a2e538b7915c2a1c01dd45cb6
Diego Iribarren <76135160+Diribar@users.noreply.github.com>
2025-10-16 14:00:25 -0300

    Avance del proyecto

---
+++ baseDatos/0-auxiliares/AUX_statusRegistros.mjs
@@ -0,0 +1,14 @@
+export default (sequelize, dt) => {
+	const alias = "statusRegistros";
+	const columns = {
+		orden: { type: dt.INTEGER },
+		nombre: { type: dt.STRING(20) },
+		codigo: { type: dt.STRING(20) },
+	};
+	const config = {
+		tableName: "aux_status_registros",
+		timestamps: false,
+	};
+	const entidad = sequelize.define(alias, columns, config);
+	return entidad;
+};
