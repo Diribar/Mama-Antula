d83b162b2b41dbb20569b6fe8809415c5b6ba171
Diego Iribarren <76135160+Diribar@users.noreply.github.com>
2025-10-17 07:51:04 -0300

    Avance del proyecto

--- baseDatos/1-niveles/NIV_1secciones.mjs
+++ baseDatos/1-niveles/NIV_1secciones.mjs
@@ -1,12 +1,16 @@
 export default (sequelize, dt) => {
-	const alias = "statusRegistros";
+	const alias = "secciones";
 	const columns = {
+		// Referencias
 		orden: {type: dt.INTEGER},
+
+		// Nombres
 		nombre: {type: dt.STRING(20)},
 		codigo: {type: dt.STRING(20)},
+		link: {type: dt.STRING(30)},
 	};
 	const config = {
-		tableName: "aux_status_registros",
+		tableName: "nivel1_secciones",
 		timestamps: false,
 	};
 	const entidad = sequelize.define(alias, columns, config);
---
+++ baseDatos/1-niveles/NIV_3pestanas.mjs
@@ -0,0 +1,25 @@
+export default (sequelize, dt) => {
+	const alias = "pestanas";
+	const columns = {
+		// Referencias
+		tema_id: {type: dt.INTEGER},
+		orden: {type: dt.INTEGER},
+
+		// Nombres
+		titulo: {type: dt.STRING(30)},
+		subtitulo: {type: dt.TEXT('medium')},
+		codigo: {type: dt.STRING(20)},
+
+		// Otros
+		ordenPorFecha: {type: dt.BOOLEAN},
+	};
+	const config = {
+		tableName: "nivel3_pestanas",
+		timestamps: false,
+	};
+	const entidad = sequelize.define(alias, columns, config);
+	entidad.associate = (n) => {
+		entidad.belongsTo(n.secciones, {as: "seccion", foreignKey: "seccion_id"});
+	};
+	return entidad;
+};
