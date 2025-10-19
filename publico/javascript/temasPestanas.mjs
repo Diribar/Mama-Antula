9ab504a8a9b05d569baccf829a9ac56d0c9b1ecf
Diego Iribarren <76135160+Diribar@users.noreply.github.com>
2025-10-19 09:06:38 -0300

    Avance del proyecto

--- publico/javascript/temasPestanas.mjs
+++ publico/javascript/temasPestanas.mjs
@@ -3,6 +3,7 @@
 window.addEventListener("load", async () => {
 	const DOM = {
 		iconoTemas: document.querySelector("#menuTemas #iconoTemas"),
+		pestanas: document.querySelectorAll(".pestana"),
 	};
 	const v = {
 		...(await fetch("/api/temas-pestanas/?url=" + pathname).then((res) => res.json())),
