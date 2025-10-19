cd4b863d15d22cfb0d9b4d8e0a90618a0da84a54
Diego Iribarren <76135160+Diribar@users.noreply.github.com>
2025-10-19 09:32:33 -0300

    Avance del proyecto

--- publico/javascript/temasPestanas.mjs
+++ publico/javascript/temasPestanas.mjs
@@ -2,11 +2,12 @@
 
 window.addEventListener("load", async () => {
 	const DOM = {
-
+		tituloTema: document.querySelector("#menuTituloTema #tituloTema"),
 		iconoTema: document.querySelector("#menuTemas #iconoTemas"),
 		listadoTemas: document.querySelector("#menuTemas #listadoTemas"),
 		temas: document.querySelectorAll("#menuTemas #listadoTemas .tema"),
-		pestanas: document.querySelectorAll(".pestana"),
+		ulsPestanas: document.querySelectorAll("#contenidosTema .pestanas"),
+		pestanas: document.querySelectorAll(".pestanas .pestana"),
 	};
 	const v = {
 		...(await fetch("/api/temas-pestanas/?url=" + pathname).then((res) => res.json())),
@@ -19,7 +20,16 @@ window.addEventListener("load", async () => {
 	// Eventos - temas
 	for (const tema of DOM.temas)
 		tema.addEventListener("click", () => {
+			// Actualiza el título del tema
+			DOM.tituloTema.textContent = tema.textContent;
+
+			// Actualiza las pestañas
 			const tema_id = tema.getAttribute("data-tema");
+			for (const ulPestanas of DOM.ulsPestanas)
+				ulPestanas.classList[ulPestanas.getAttribute("data-tema_id") == tema_id ? "remove" : "add"]("ocultar");
+
+			// Fin
+			return;
 		});
 
 	// Fin
