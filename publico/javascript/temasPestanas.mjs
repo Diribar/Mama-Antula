b6f649fbff19a462435712a7818a4e03f9648ee0
Diego Iribarren <76135160+Diribar@users.noreply.github.com>
2025-10-19 14:40:20 -0300

    Avance del proyecto

--- publico/javascript/temasPestanas.mjs
+++ publico/javascript/temasPestanas.mjs
@@ -2,19 +2,24 @@
 
 window.addEventListener("load", async () => {
 	const DOM = {
+		// Menú de temas
+		iconoMenuTemas: document.querySelector("#menuTemas #iconoMenuTemas"),
+		menuTemas: document.querySelector("#menuTemas #menuTemas"),
+		temas: document.querySelectorAll("#menuTemas #menuTemas .tema"),
 		tituloTema: document.querySelector("#menuTituloTema #tituloTema"),
-		iconoTema: document.querySelector("#menuTemas #iconoTemas"),
-		listadoTemas: document.querySelector("#menuTemas #listadoTemas"),
-		temas: document.querySelectorAll("#menuTemas #listadoTemas .tema"),
+
+		// Contenido
+		contenidoTema: document.querySelectorAll("#contenidoTemas .contenidoTema"),
 		ulsPestanas: document.querySelectorAll("#contenidoTemas .pestanas"),
 		pestanas: document.querySelectorAll(".pestanas .pestana"),
+		contenidoPestanas: document.querySelectorAll(".contenidoPestanas"),
 	};
 	const v = {
 		...(await fetch("/api/temas-pestanas/?url=" + pathname).then((res) => res.json())),
 	};
 
-	// Eventos - iconoTema
-	DOM.iconoTema.addEventListener("click", () => DOM.listadoTemas.classList.toggle("ocultar"));
+	// Eventos - iconoMenuTemas
+	DOM.iconoMenuTemas.addEventListener("click", () => DOM.menuTemas.classList.toggle("ocultar"));
 
 	// Eventos - temas
 	for (const tema of DOM.temas)
@@ -22,7 +27,11 @@ window.addEventListener("load", async () => {
 			// Actualiza el título del tema
 			DOM.tituloTema.textContent = tema.textContent;
 
-			// Actualiza las pestañas
+			// Actualiza el contenido visible del tema
+			// for ()
+
+
+			// Actualiza las pestañas del tema
 			const tema_id = tema.getAttribute("data-tema");
 			for (const ulPestanas of DOM.ulsPestanas)
 				ulPestanas.classList[ulPestanas.getAttribute("data-tema_id") == tema_id ? "remove" : "add"]("ocultar");
