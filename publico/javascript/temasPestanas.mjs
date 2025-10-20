693085e457e562f4417e2b2403523f7835e31c55
Diego Iribarren <76135160+Diribar@users.noreply.github.com>
2025-10-20 07:56:52 -0300

    Avance del proyecto

--- publico/javascript/temasPestanas.mjs
+++ publico/javascript/temasPestanas.mjs
@@ -14,7 +14,7 @@ window.addEventListener("load", async () => {
 		listadosPestanas: document.querySelectorAll("#contenidoTemas .listadoPestanas"),
 		contenidoPestanas: document.querySelectorAll(".contenidoPestana"),
 	};
-	v.seccionCodigo = document.querySelector("#menuTituloTema").getAttribute("data-seccionCodigo");
+	v.seccionCodigo = document.querySelector("#menuTituloTema").getAttribute("data-seccion_codigo");
 
 	// Eventos - temas
 	for (const tema of DOM.temas)
@@ -29,12 +29,16 @@ window.addEventListener("load", async () => {
 			DOM.tituloTema.textContent = tema.textContent;
 
 			// Muestra el listado de pestañas del tema activo, y oculta el listado de los demás temas
-			for (const listadoPestanas of DOM.listadosPestanas)
-				listadoPestanas.classList[listadoPestanas.getAttribute("data-tema_codigo") == v.temaCodigo ? "remove" : "add"]("ocultar");
+			for (const listado of DOM.listadosPestanas)
+				listado.classList[listado.getAttribute("data-tema_codigo") == v.temaCodigo ? "remove" : "add"]("ocultar");
 
 			// Muestra el contenido del tema activo, y oculta el de los demás temas
-			for (const contenidoTema of DOM.contenidosTema)
-				contenidoTema.classList[contenidoTema.getAttribute("data-tema_codigo") == v.temaCodigo ? "remove" : "add"]("ocultar");
+			for (const contenido of DOM.contenidosTema)
+				contenido.classList[contenido.getAttribute("data-tema_codigo") == v.temaCodigo ? "remove" : "add"]("ocultar");
+
+			// Muestra el contenido de la pestana activa, y oculta el de las demás
+			for (const cont of DOM.contenidoPestanas)
+				cont.classList[cont.getAttribute("data-pestana_codigo") == v.pestanaCodigo ? "remove" : "add"]("ocultar");
 
 			// Fin
 			return;
@@ -42,25 +46,25 @@ window.addEventListener("load", async () => {
 
 	// Eventos - pestañas
 	for (const listadoPestanas of DOM.listadosPestanas) {
+		// Obtiene las pestañas del listado
 		const pestanas = listadoPestanas.querySelectorAll(".pestana");
+
+		// Acciones cuando se cambia de pestaña
 		for (const pestanaActiva of pestanas)
 			pestanaActiva.addEventListener("click", () => {
 				// Variables
-				v.pestanaCodigo = tema.getAttribute("data-pestana_codigo");
+				v.pestanaCodigo = pestanaActiva.getAttribute("data-pestana_codigo");
 
 				// Actualiza la cookie de pestana
 				document.cookie = v.temaCodigo + "=" + v.pestanaCodigo;
 
 				// Activa la pestaña actual, y desactiva las demás
-				for (const pestana of pestanas)
-					pestana.classList[pestana.getAttribute("data-pestana_codigo") == v.pestanaCodigo ? "add" : "remove"]("activo");
+				for (const pest of pestanas)
+					pest.classList[pest.getAttribute("data-pestana_codigo") == v.pestanaCodigo ? "add" : "remove"]("activo");
 
 				// Muestra el contenido de la pestaña activa, y oculta el de las demás
-				for (const contenidoPestana of DOM.contenidoPestanas) {
-					contenidoPestana.classList[contenidoPestana.getAttribute("data-pestana_codigo") == v.pestanaCodigo ? "remove" : "add"](
-						"ocultar"
-					);
-				}
+				for (const cont of DOM.contenidoPestanas)
+					cont.classList[cont.getAttribute("data-pestana_codigo") == v.pestanaCodigo ? "remove" : "add"]("ocultar");
 			});
 	}
 
