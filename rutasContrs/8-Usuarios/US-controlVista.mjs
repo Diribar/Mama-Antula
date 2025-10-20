246f3edda5c84b889de642263c9b0f4d798620f5
Diego Iribarren <76135160+Diribar@users.noreply.github.com>
2025-10-20 17:05:52 -0300

    Avance del proyecto

--- rutasContrs/8-Usuarios/US-controlVista.mjs
+++ rutasContrs/8-Usuarios/US-controlVista.mjs
@@ -5,15 +5,23 @@ export default {
 	abm: async (req, res) => {
 		// Variables
 		const tituloPagina = "ABM de Usuarios";
+		const tema = "usuarios";
+		const codigo = "abmUsuarios";
 
 		// Fin
-		return res.render("CMP-0Estructura", {tituloPagina});
+		return res.render("CMP-0Estructura", {
+			...{tituloPagina, tema, codigo},
+		});
 	},
 	cambioRol: async (req, res) => {
 		// Variables
 		const tituloPagina = "Cambio de Rol en Usuarios";
+		const tema = "usuarios";
+		const codigo = "cambioRolUsuarios";
 
 		// Fin
-		return res.render("CMP-0Estructura", {tituloPagina});
+		return res.render("CMP-0Estructura", {
+			...{tituloPagina, tema, codigo},
+		});
 	},
 };
