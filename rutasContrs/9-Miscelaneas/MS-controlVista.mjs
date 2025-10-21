6fe286fb873ae7386483151a272244aadf8cdab9
Diego Iribarren <76135160+Diribar@users.noreply.github.com>
2025-10-21 15:18:33 -0300

    Solución de errores

--- rutasContrs/9-Miscelaneas/MS-controlVista.mjs
+++ rutasContrs/9-Miscelaneas/MS-controlVista.mjs
@@ -1,27 +1,26 @@
 "use strict";
 // import procesos from "./US-procesos.mjs";
+const temaVista = "miscelaneas";
 
 export default {
 	contactanos: async (req, res) => {
 		// Variables
 		const tituloPagina = "Contactanos";
-		const tema = "miscelaneas";
 		const codigo = "contactanos";
 
 		// Fin
 		return res.render("CMP-0Estructura", {
-			...{tituloPagina, tema, codigo},
+			...{tituloPagina, temaVista, codigo},
 		});
 	},
 	estadisticas: async (req, res) => {
 		// Variables
 		const tituloPagina = "Estadísticas";
-		const tema = "miscelaneas";
 		const codigo = "estadisticas";
 
 		// Fin
 		return res.render("CMP-0Estructura", {
-			...{tituloPagina, tema, codigo},
+			...{tituloPagina, temaVista, codigo},
 		});
 	},
 };
