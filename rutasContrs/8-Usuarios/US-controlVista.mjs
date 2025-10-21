02d30682987e8ce6d539b7dca37d09471a76af1c
Diego Iribarren <76135160+Diribar@users.noreply.github.com>
2025-10-21 15:13:27 -0300

    Avance del proyecto

--- rutasContrs/8-Usuarios/US-controlVista.mjs
+++ rutasContrs/8-Usuarios/US-controlVista.mjs
@@ -1,38 +1,36 @@
 "use strict";
 // import procesos from "./US-procesos.mjs";
+const temaVista = "usuarios";
 
 export default {
 	altaOlvido: async (req, res) => {
 		// Variables
 		const tituloPagina = "Alta de Usuario / Olvido de Contraseña";
-		const tema = "usuarios";
 		const codigo = "altaOlvido";
 
 		// Fin
 		return res.render("CMP-0Estructura", {
-			...{tituloPagina, tema, codigo},
+			...{tituloPagina, temaVista, codigo},
 		});
 	},
 	edicion: async (req, res) => {
 		// Variables
 		const tituloPagina = "Edición de Usuario";
-		const tema = "usuarios";
 		const codigo = "edicion";
 
 		// Fin
 		return res.render("CMP-0Estructura", {
-			...{tituloPagina, tema, codigo},
+			...{tituloPagina, temaVista, codigo},
 		});
 	},
 	cambioRoles: async (req, res) => {
 		// Variables
 		const tituloPagina = "Cambio de Roles";
-		const tema = "usuarios";
 		const codigo = "cambioRolUsuarios";
 
 		// Fin
 		return res.render("CMP-0Estructura", {
-			...{tituloPagina, tema, codigo},
+			...{tituloPagina, temaVista, codigo},
 		});
 	},
 };
