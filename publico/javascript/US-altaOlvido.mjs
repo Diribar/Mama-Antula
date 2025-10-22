9a1507964676d25decbf7101bcf204bd20763f1f
Diego Iribarren <76135160+Diribar@users.noreply.github.com>
2025-10-22 10:54:10 -0300

    Avance del proyecto

--- publico/javascript/US-altaOlvido.mjs
+++ publico/javascript/US-altaOlvido.mjs
@@ -7,4 +7,26 @@ window.addEventListener("load", async () => {
 		mensajeError: document.querySelector("#formAltaOlvido #mensajeError"),
 		confirma: document.querySelector("#formAltaOlvido button"),
 	};
+
+	// Eventos - 'input'
+	DOM.form.addEventListener("input", () => (DOM.mensajeError.innerHTML = ""));
+	DOM.form.addEventListener("change", async (e) => {
+		// Variables
+		const {name: campo, value: valor} = e.target;
+
+		// Garantiza que el mail esté en minúsculas
+		e.target.value = valor.toLowerCase();
+
+		// Actualiza los errores
+		errorMail = !valor ? cartelMailVacio : !formatoMail.test(valor) ? cartelMailFormato : "";
+
+		// Actualiza la vista
+		FN.actualizaLosErrores();
+
+		// Fin
+		return;
+	});
 });
+
+// Variables
+let errorMail;
