21a96d3f7dc538c57a9e99d3b60a7af15d23be8b
Diego Iribarren <76135160+Diribar@users.noreply.github.com>
2025-10-22 11:14:03 -0300

    Avance del proyecto

--- publico/javascript/US-altaOlvido.mjs
+++ publico/javascript/US-altaOlvido.mjs
@@ -8,25 +8,51 @@ window.addEventListener("load", async () => {
 		confirma: document.querySelector("#formAltaOlvido button"),
 	};
 
-	// Eventos - 'input'
-	DOM.form.addEventListener("input", () => (DOM.mensajeError.innerHTML = ""));
+	// Eventos - input
+	DOM.form.addEventListener("input", () => {
+		DOM.mensajeError.innerHTML = "";
+		DOM.mensajeError.classList.remove("error");
+		DOM.confirma.classList.remove("inactivo");
+	});
+	// Eventos - change
 	DOM.form.addEventListener("change", async (e) => {
-		// Variables
-		const {name: campo, value: valor} = e.target;
-
 		// Garantiza que el mail esté en minúsculas
-		e.target.value = valor.toLowerCase();
+		const valor = e.target.value.toLowerCase();
+		e.target.value = valor;
 
-		// Actualiza los errores
+		// Averigua si hay un error simple
 		errorMail = !valor ? cartelMailVacio : !formatoMail.test(valor) ? cartelMailFormato : "";
 
-		// Actualiza la vista
-		FN.actualizaLosErrores();
+		// Acciones si hay un error
+		if (errorMail) {
+			DOM.mensajeError.innerHTML = errorMail;
+			DOM.mensajeError.classList.add("error");
+			DOM.confirma.classList.add("inactivo");
+		}
 
 		// Fin
 		return;
 	});
+	// Eventos - submit
+	DOM.form.addEventListener("submit", async (e) => {
+		// Si existe un error, interrumpe la función
+		e.preventDefault();
+		DOM.confirma.classList.add("inactivo");
+		if (errorMail) return;
+
+		// Envía el mail al backend
+		const respuesta = await fetch(rutaApi + DOM.email.value).then((n) => n.json());
+
+		// Acciones en función de la respuesta recibida
+		DOM.confirma.classList[respuesta.hay ? "add" : "remove"]("error");
+		DOM.confirma.classList[!respuesta.hay ? "add" : "remove"]("OK");
+		DOM.mensajeError.innerHTML = respuesta.mensaje;
+	});
 });
 
 // Variables
+const cartelMailVacio = "Necesitamos que escribas un correo electrónico";
+const cartelMailFormato = "Debes escribir un formato de correo válido";
+const formatoMail = /^\w+([\.-_]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+const rutaApi = "/usuarios/api/us-alta-de-mail-u-olvido-de-contrasena/?email=";
 let errorMail;
