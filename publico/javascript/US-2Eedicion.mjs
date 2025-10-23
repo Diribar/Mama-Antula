28a2e5b9e8ddc94a8471ff5a81fdf9608a00f6d9
Diego Iribarren <76135160+Diribar@users.noreply.github.com>
2025-10-23 01:07:19 -0300

    Avance del proyecto

---
+++ publico/javascript/US-2Eedicion.mjs
@@ -0,0 +1,67 @@
+"use strict";
+
+window.addEventListener("load", async () => {
+	const DOM = {
+		form: document.querySelector("#contenidoTemas #formAltaOlvido"),
+		email: document.querySelector("#formAltaOlvido input[name='email']"),
+		mensaje: document.querySelector("#formAltaOlvido #mensaje"),
+		confirma: document.querySelector("#formAltaOlvido #confirma"),
+	};
+	const rutaApi = "/usuarios/api/us-alta-de-mail-u-olvido-de-contrasena/?email=";
+
+	const fnErrorMail = () => {
+		// Garantiza que el mail esté en minúsculas
+		const valor = DOM.email.value.toLowerCase();
+		DOM.email.value = valor;
+
+		// Averigua si hay un error simple
+		errorMail = !valor ? cartelMailVacio : !formatoMail.test(valor) ? cartelMailFormato : "";
+		console.log(errorMail, valor);
+
+		// Acciones si hay un error
+		if (errorMail) {
+			DOM.mensaje.innerHTML = errorMail;
+			DOM.mensaje.classList.add("error");
+			DOM.confirma.classList.add("inactivo");
+		}
+	};
+
+	// Eventos - input
+	DOM.form.addEventListener("input", () => {
+		DOM.mensaje.innerHTML = "";
+		DOM.mensaje.classList.remove("error");
+		DOM.confirma.classList.remove("inactivo");
+	});
+	// Eventos - change
+	DOM.form.addEventListener("change", async () => fnErrorMail());
+	// Eventos - submit
+	DOM.form.addEventListener("submit", async (e) => {
+		// Evita el confirm
+		e.preventDefault();
+
+		// Si no se revisaron los errores, los revisa
+		if (errorMail === undefined) fnErrorMail();
+
+		// Si existe un error, interrumpe la función
+		if (errorMail) return;
+		if (DOM.confirma.className.includes("inactivo")) return;
+		DOM.confirma.classList.add("inactivo");
+
+		// Envía el mail al backend
+		const respuesta = await fetch(rutaApi + DOM.email.value).then((n) => n.json());
+
+		// Acciones en función de la respuesta recibida
+		DOM.mensaje.classList[!respuesta.hay ? "add" : "remove"]("exito");
+		DOM.mensaje.classList[respuesta.hay ? "add" : "remove"]("error");
+		DOM.mensaje.innerHTML = respuesta.email || respuesta.mensaje;
+
+		// Fin
+		return;
+	});
+});
+
+// Variables
+const cartelMailVacio = "Necesitamos que escribas un correo electrónico";
+const cartelMailFormato = "Debes escribir un formato de correo válido";
+const formatoMail = /^\w+([\.-_]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+let errorMail;
