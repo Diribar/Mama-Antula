07c72d7425fa9264c2c875055311965d29bbbbf9
Diego Iribarren <76135160+Diribar@users.noreply.github.com>
2025-10-23 01:03:25 -0300

    Avance del proyecto

--- rutasContrs/8-Usuarios/US-rutas.mjs
+++ rutasContrs/8-Usuarios/US-rutas.mjs
@@ -1,6 +1,12 @@
+// Imports
 import express from "express";
 import API from "./US-controlApi.mjs";
 import vista from "./US-controlVista.mjs";
+
+// Middlewares
+import soloVisitas from "../../middlewares/permisosUsuario/US-1SoloVisitas.mjs";
+import soloUsuarios from "../../middlewares/permisosUsuario/US-2SoloUsuarios.mjs";
+import soloAdmin from "../../middlewares/permisosUsuario/US-3SoloAdmin.mjs";
 
 // 🧩 Router
 const router = express.Router();
@@ -11,9 +17,9 @@ router.get("/api/us-logout", API.logout);
 router.get("/api/us-alta-de-mail-u-olvido-de-contrasena", API.altaOlvido);
 
 // 🖥️ Vistas
-router.get(rutasUsuario.altaOlvido, vista.altaOlvido);
-router.get(rutasUsuario.edicion, vista.edicion);
-router.get("/cambio-de-roles", vista.cambioRoles);
+router.get(rutasUsuario.altaOlvido, soloVisitas, vista.altaOlvido);
+router.get(rutasUsuario.edicion, soloUsuarios, vista.edicion);
+router.get(rutasUsuario.cambioRoles, soloAdmin, vista.cambioRoles);
 
 // ✅ Export
 export default router;
