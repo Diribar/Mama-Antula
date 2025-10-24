1e65dc62e08889d720177f58db428db2e059c6f5
Diego Iribarren <76135160+Diribar@users.noreply.github.com>
2025-10-24 15:05:16 -0300

    Avance del proyecto

--- publico/javascript/0-Compartido/CMP-DragDrop.mjs
+++ publico/javascript/0-Compartido/CMP-DragDrop.mjs
@@ -1,27 +1,32 @@
 const procesaArchImg = (files) => {
-	// Si no hay una imagen, interrumpe la función
-	if (!files.length) return;
+	return new Promise((resolve, reject) => {
+		// Si no hay una imagen, interrumpe la función
+		if (!files.length) return resolve(null);
 
-	// Si no tiene extensión de imagen o supera el tamaño maximo permitido, interrumpe la función
-	const file = files[0];
-	const maxBytes = 1024000; // 1 MB
-	if (!file.type.startsWith("image/") || file.size > maxBytes) return file;
+		// Si no tiene extensión de imagen o supera el tamaño maximo permitido, interrumpe la función
+		const file = files[0];
+		const maxBytes = 1024000; // 1 MB
+		if (!file.type.startsWith("image/") || file.size > maxBytes) return resolve(file);
 
-	// Lee el archivo
-	const reader = new FileReader();
-	reader.readAsDataURL(file);
-	reader.onload = () => {
+		// Lee el archivo
+		const reader = new FileReader();
+		reader.readAsDataURL(file);
+		reader.onload = () => {
 		// Valida que el contenido sea una imagen (renderizable)
-		const image = new Image();
-		image.src = reader.result;
+			const image = new Image();
+			image.src = reader.result;
 
-		// Acciones si realmente es una imagen
-		image.onload = async () => {
-			DOM.vistaImagen.src = reader.result; // hace visible la imagen
-			return file;
+			// Acciones si realmente es una imagen
+			image.onload = () => {
+				DOM.vistaImagen.src = reader.result; // hace visible la imagen
+				return resolve(file); // Resuelve la promesa con el archivo
+			};
+
+			// Si no es imagen, resuelve con null
+			image.onerror = () => resolve(null);
 		};
 
-		// Acciones si no es una imagen
-		image.onerror = async () => null;
-	};
+		// Si no se pudo leer el archivo, resuelve con null
+		reader.onerror = () => resolve(null);
+	});
 };
