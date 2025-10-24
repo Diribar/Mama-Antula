4e547dece0fbe007fe14694dabeb6c56426ae788
Diego Iribarren <76135160+Diribar@users.noreply.github.com>
2025-10-24 08:00:08 -0300

    Avance del proyecto

--- funciones/compartidas.mjs
+++ funciones/compartidas.mjs
@@ -53,4 +53,57 @@ export default {
 		// Fin
 		return mailEnviado;
 	},
+	validacs: {
+		longitud: (dato, corto, largo) => {
+			return dato.length < corto
+				? "El contenido debe ser más largo"
+				: dato.length > largo
+				? "El contenido debe ser más corto"
+				: "";
+		},
+		castellano: {
+			basico: (dato) => {
+				let formato = /^[a-záéíóúüñ ,.'\-]+$/i;
+				return !formato.test(dato) ? "Sólo se admiten letras del abecedario castellano" : "";
+			},
+			completo: (dato) => {
+				let formato = /^[a-záéíóúüñ ,.'&$:;…"°¿?¡!+/()\d\-]+$/i;
+				return !formato.test(dato) ? "Sólo se admiten letras del abecedario castellano" : "";
+			},
+		},
+		inicial: {
+			basico: (dato) => {
+				let formato = /^[A-ZÁÉÍÓÚÜÑ]/;
+				return !formato.test(dato) ? "La primera letra debe ser en mayúscula" : "";
+			},
+			completo: (dato) => {
+				let formato = /^[A-ZÁÉÍÓÚÜÑ¡¿"\d]/;
+				return !formato.test(dato) ? "La primera letra debe ser en mayúscula" : "";
+			},
+			sinopsis: (dato) => {
+				let formato = /^[A-ZÁÉÍÓÚÜÑ¡¿"\d]/;
+				return !formato.test(dato) ? "La primera letra debe ser en mayúscula" : "";
+			},
+		},
+		imagen: (datos) => {
+			// Variables
+			const {imagen, tamano, tipo} = datos;
+			const ext = path.extname(imagen).toLowerCase().slice(1);
+			const extValidas = ["jpg", "png", "jpeg"];
+
+			// Valida la extensión y el tamaño
+			const respuesta = !ext // Valida la extensión
+				? "El archivo debe tener alguna extensión"
+				: !extValidas.includes(ext) || !extValidas.some((n) => tipo.endsWith(n))
+				? "Usaste un archivo con la extensión " + ext + ". Las extensiones válidas son " + extValidas.join(", ")
+				: !tipo.startsWith("image/")
+				? "El archivo debe ser de tipo imagen"
+				: tamano && tamano > tamMaxImagen // Valida el tamaño
+				? "El archivo tiene " + Math.ceil((tamano / Mb1) * 100) / 100 + " MB. Necesitamos que no supere 1 MB"
+				: "";
+
+			// Fin
+			return respuesta;
+		},
+	},
 };
