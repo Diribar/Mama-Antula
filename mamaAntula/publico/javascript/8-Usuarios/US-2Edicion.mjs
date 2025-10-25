75cb7acc45956c1ba2accbce834db671e84c3f7a
Diego Iribarren <76135160+Diribar@users.noreply.github.com>
2025-10-25 11:33:18 -0300

    Solución de errores

--- mamaAntula/publico/javascript/8-Usuarios/US-2Edicion.mjs
+++ mamaAntula/publico/javascript/8-Usuarios/US-2Edicion.mjs
@@ -175,10 +175,10 @@ window.addEventListener("load", async () => {
 		DOM.confirma.classList.add("inactivo"); // se deja inactivo hasta que se vuelve a hacer un input en el formulario
 
 		// Si no hay algo para guardar, interrumpe la función
-		if (!accionesSubmit.hayAlgoParaGuardar()) return ;
+		if (!FN.accionesSubmit.hayAlgoParaGuardar()) return ;
 
 		// Crea el FormData y agrega los datos
-		const formData = accionesSubmit.formData();
+		const formData = FN.accionesSubmit.formData();
 
 		// Valida y guarda los cambios del form
 		v.errores = await fetch(v.rutaGuardar, fnUsuariosComp.postForm(formData)).then((n) => n.json());
