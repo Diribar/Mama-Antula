3b8904b9e68c01e82566a5f46b2e60ec59b06060
Diego Iribarren <76135160+Diribar@users.noreply.github.com>
2025-10-25 12:26:24 -0300

    Avance del proyecto

--- mamaAntula/middlewares/transversales/CMP-mailValidado.mjs
+++ mamaAntula/middlewares/transversales/CMP-mailValidado.mjs
@@ -6,8 +6,12 @@ export default async (req, res, next) => {
 	let redireccionEdicionUsuario;
 
 	// Si el usuario está en status mailValidado, debe pasar al status conApodo
-	if (usuario && usuario.statusRegistro_id == mailValidado_id && !req.originalUrl.startsWith("/usuarios/edicionUsuario"))
-		return res.redirect("/usuarios/edicionUsuario");
+	if (
+		usuario &&
+		usuario.statusRegistro_id == mailValidado_id &&
+		!req.originalUrl.startsWith("/usuarios" + rutasUsuario.edicion)
+	)
+		return res.redirect("/usuarios" + rutasUsuario.edicion);
 
 	// Fin
 	return next();
