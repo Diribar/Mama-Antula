6f295ef287426ab023cfcfacff6aca5ff834518f
Diego Iribarren <76135160+Diribar@users.noreply.github.com>
2025-10-28 08:28:50 -0300

    Avance del proyecto

--- mamaAntula/middlewares/LT-validaRuta.mjs
+++ mamaAntula/middlewares/LT-validaRuta.mjs
@@ -1,35 +1,43 @@
 export default (req, res, next) => {
 	// Variables
 	const {urlSeccion, urlTema, urlPestana} = req.params;
-
 	const informacion = {mensajes: ["No tenemos esa dirección en nuestro sistema"]};
 
-	// Si el urlSeccion no existe, redirige
+	// SECCION - Si el urlSeccion no existe, redirige
 	if (!urlSeccion) {
+		const {seccionActual} = req.cookies || "inicio";
+		const urlActual = secciones.find((n) => n.url == seccionActual).url;
+		return res.redirect("/" + urlActual);
 	}
 
-	// Si la seccionActual no corresponde, muestra la vista de error
+	// SECCION - Si la seccionActual no corresponde, muestra la vista de error
 	const seccionActual = secciones.find((n) => n.url == urlSeccion);
 	if (!seccionActual) return res.render("CMP-0Estructura", {informacion});
 
-	// Si el urlTema no existe redirige
+
+	// TEMA - Si el urlTema no existe, redirige
+	const temasPosibles = temasSecciones.filter((n) => n.seccion_id == seccionActual.id);
 	if (!urlTema) {
+		const urlActual = temasPosibles.find((n) => n.codigo === req.cookies.temaActual)?.url || temasPosibles[0].url;
+		return res.redirect("/" + seccionActual.url + "/" + urlActual);
 	}
 
-	// Si el temaActual no corresponde, muestra la vista de error
-	const temaActual = temasSecciones.find((n) => n.url == urlTema);
-	if (!temaActual || temaActual.seccion_id != seccionActual.id) return res.render("CMP-0Estructura", {informacion});
+	// TEMA - Si el temaActual no corresponde, muestra la vista de error
+	const temaActual = temasPosibles.find((n) => n.url == urlTema);
+	if (!temaActual) return res.render("CMP-0Estructura", {informacion});
 
-	// Averigua si el tema tiene pestañas
+	// PESTAÑA - Averigua si el tema tiene pestañas
 	const pestanasPosibles = pestanasTemas.filter((n) => n.tema_id == temaActual.id);
 	if (pestanasPosibles.length) {
 		// Si el urlPestana no existe redirige
 		if (!urlPestana) {
+			const urlActual = pestanasPosibles.find((n) => n.codigo == req.cookies.pestanaActual)?.url || pestanasPosibles[0].url;
+			return res.redirect("/" + seccionActual.url + "/" + temaActual.url + "/" + urlActual);
 		}
 
 		// Si la pestanaActual no corresponde, muestra la vista de error
-		const pestanaActual = pestanasTemas.find((n) => n.url == urlTema);
-		if (!pestanaActual || pestanaActual.tema_id != temaActual.id) return res.render("CMP-0Estructura", {informacion});
+		const pestanaActual = pestanasPosibles.find((n) => n.url == urlPestana);
+		if (!pestanaActual) return res.render("CMP-0Estructura", {informacion});
 	}
 
 	// Fin
