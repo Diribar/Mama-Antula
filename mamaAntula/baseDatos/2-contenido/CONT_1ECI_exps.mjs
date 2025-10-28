6bfa2096b82188f8dc6d867e3935b9250063caa1
Diego Iribarren <76135160+Diribar@users.noreply.github.com>
2025-10-28 17:32:01 -0300

    Avance del proyecto

--- mamaAntula/baseDatos/2-contenido/CONT_1ECI_exps.mjs
+++ mamaAntula/baseDatos/2-contenido/CONT_1ECI_exps.mjs
@@ -18,11 +18,8 @@ export default (sequelize, dt) => {
 	};
 	const entidad = sequelize.define(alias, columns, config);
 	entidad.associate = (n) => {
-		entidad.belongsTo(n.personajes, {as: "nombreDesde", foreignKey: "nombreDesde_id"});
-		entidad.belongsTo(n.personajes, {as: "nombreHacia", foreignKey: "nombreHacia_id"});
 		entidad.belongsTo(n.lugares, {as: "lugar", foreignKey: "lugar_id"});
-		entidad.belongsTo(n.idiomas, {as: "idioma", foreignKey: "idioma_id"});
-		entidad.belongsTo(n.usuarios, {as: "usuario", foreignKey: "creadoPor_id"});
+		entidad.belongsTo(n.usuarios, {as: "creadoPor", foreignKey: "creadoPor_id"});
 		entidad.belongsTo(n.statusRegistros, {as: "statusRegistro", foreignKey: "statusRegistro_id"});
 	};
 	return entidad;
