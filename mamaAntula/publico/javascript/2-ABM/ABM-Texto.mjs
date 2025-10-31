b03f96813757da47c479d1c9d8ec2358efa45d13
Diego Iribarren <76135160+Diribar@users.noreply.github.com>
2025-10-31 08:54:19 -0300

    Avance del proyecto

--- mamaAntula/publico/javascript/2-ABM/ABM-Texto.mjs
+++ mamaAntula/publico/javascript/2-ABM/ABM-Texto.mjs
@@ -2,30 +2,56 @@
 
 window.addEventListener("load", async () => {
 	// Variables
-	const barraHerrams = {
-		grupo1: [
-			{codigo: "negrita", clase: "ql-bold", title: "Negrita"},
-			{codigo: "cursiva", clase: "ql-italic", title: "Cursiva"},
-		],
-		grupo2: [
-			{codigo: "listaNum", clase: "ql-list", value: "ordered", title: "Lista numerada"},
-			{codigo: "dots", clase: "ql-list", value: "bullet", title: "Lista con viñetas"},
-			{codigo: "cita", clase: "ql-blockquote", title: "Cita"},
-		],
-		grupo3: [{codigo: "link", clase: "ql-link", title: "Insertar enlace"}],
-		grupo4: [{codigo: "limpiar", clase: "ql-clean", title: "Quitar formato"}],
+	const DOM = {
+		barraHerrams: document.querySelector("#barraHerrams-texto"),
 	};
-	console.log(Object.values(barraHerrams).flat().map(n=>n.clase.replace("ql-", "")));
+	const v = {
+		barraHerrams: {
+			grupo1: [
+				{codigo: "negrita", clase: "ql-bold", title: "Negrita"},
+				{codigo: "cursiva", clase: "ql-italic", title: "Cursiva"},
+			],
+			grupo2: [
+				{codigo: "listaNum", clase: "ql-list", valor: "ordered", title: "Lista numerada"},
+				{codigo: "dots", clase: "ql-list", valor: "bullet", title: "Lista con viñetas"},
+				{codigo: "cita", clase: "ql-blockquote", title: "Cita"},
+			],
+			grupo3: [{codigo: "link", clase: "ql-link", title: "Insertar enlace"}],
+			grupo4: [{codigo: "limpiar", clase: "ql-clean", title: "Quitar formato"}],
+		},
+	};
 
+	// Obtiene el nombre de las etiquetas
+	const nombreEtiquetas = Object.values(v.barraHerrams)
+		.flat()
+		.map((n) => n.clase.replace("ql-", ""));
 
-	// Inicializamos Quill
-	const quill = new Quill("#editor-container", {
-		theme: "snow",
-		placeholder: "Escribí acá tu contenido...",
-		modules: {toolbar: "#toolbar"},
-		formats: Object.values(barraHerrams).flat().map(n=>n.clase.replace("ql-", "")),
-		//["bold", "italic", "color", "list", "blockquote", "link", "image", "video"],
-	});
+	// Arma la barra de herramientas
+	for (const grupo in v.barraHerrams) {
+		// Agrega el contenedor
+		const span = document.createElement("span");
+		span.classList.add("ql-formats");
+		DOM.barraHerrams.appendChild(span);
+
+		// Crea los botones
+		for (const herramienta of v.barraHerrams[grupo]) {
+			// Crea el botón
+			const button = document.createElement("button");
+			button.classList.add(herramienta.clase);
+			if (herramienta.valor) button.setAttribute("value", herramienta.valor);
+			button.title = herramienta.title;
+
+			// Agrega el boton
+			//button.addEventListener("click", () => quill.format(herramienta.clase, herramienta.valor));
+			span.appendChild(button);
+		}
+	}
+
+	// Funciones - Inicializamos Quill
+	const formats = [...new Set(nombreEtiquetas)];
+	const placeholder = "Escribí acá tu contenido...";
+	const toolbar = "#barraHerrams-texto";
+	const quill = new Quill("#edicion-texto", {theme: "snow", placeholder, modules: {toolbar}, formats});
 
 	// Función para sincronizar textarea y previsualización
 	const actualizarContenido = () => {
@@ -34,24 +60,18 @@ window.addEventListener("load", async () => {
 			.replace(/\s+/g, " ") // reemplaza espacios duplicados
 			.replace(" </", "</") // reemplaza espacios mal puestos
 			.trim(); // reemplaza espacios al final
-		document.getElementById("contenidoText").value = html;
+		//document.getElementById("contenidoText").value = html;
 		return;
 	};
 
 	// EVENTO - Escuchamos cambios en Quill
 	quill.on("text-change", actualizarContenido);
 
-	// Evitar que se peguen estilos de color al pegar
-	quill.clipboard.addMatcher(Node.ELEMENT_NODE, (node, delta) => {
-		delta.ops.forEach((op) => op.attributes && delete op.attributes.color);
-		return delta;
-	});
-
 	// Capturamos submit
-	document.getElementById("miForm").addEventListener("submit", function (e) {
-		e.preventDefault();
-		console.log("Título:", document.getElementById("titulo").value);
-		console.log("Contenido HTML:", document.getElementById("contenidoText").value);
-		alert("Formulario listo para enviar. Revisa la consola.");
-	});
+	// document.getElementById("miForm").addEventListener("submit", function (e) {
+	// 	e.preventDefault();
+	// 	console.log("Título:", document.getElementById("titulo").value);
+	// 	console.log("Contenido HTML:", document.getElementById("contenidoText").value);
+	// 	alert("Formulario listo para enviar. Revisa la consola.");
+	// });
 });
