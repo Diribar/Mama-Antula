02ccb09c59aa25ef7780d3c6f0a69440115cf043
Diego Iribarren <76135160+Diribar@users.noreply.github.com>
2025-11-04 17:42:15 -0300

    Avance del proyecto

--- mamaAntula/publico/javascript/2-ABM-Actualizar/ABM-1Filtros.mjs
+++ mamaAntula/publico/javascript/2-ABM-Actualizar/ABM-1Filtros.mjs
@@ -15,7 +15,7 @@ window.addEventListener("load", async () => {
 		obtieneEncabs: "/contenido/api/abm-obtiene-encabezados/?",
 	};
 	cac = await fetch(rutas.datosIniciales).then((n) => n && n.json());
-	const v = {startUp: true};
+	const v = {};
 
 	// Funciones
 	const FN = {
@@ -47,10 +47,7 @@ window.addEventListener("load", async () => {
 			if (cac.tipoEncab != "encabSinIndice") DOM.encabezado.classList.remove("ocultar");
 
 			// ENCABEZADO - Si es start-up, elige la opción de la cookie
-			if (v.startUp) {
-				if (cookie("actualizaEncabezado_id")) DOM.encabezado.value = cookie("actualizaEncabezado_id");
-				delete v.startUp;
-			}
+			if (cac.startUp && cookie("actualizaEncabezado_id")) DOM.encabezado.value = cookie("actualizaEncabezado_id");
 
 			DOM.encabezado.dispatchEvent(new Event("change"));
 
@@ -74,11 +71,11 @@ window.addEventListener("load", async () => {
 	// Eventos de filtros
 	DOM.seccion.addEventListener("change", () => {
 		// SECCION - Si es start-up, elige la opción de la cookie
-		if (v.startUp && cookie("actualizaSeccion_id")) DOM.seccion.value = cookie("actualizaSeccion_id");
+		if (cac.startUp && cookie("actualizaSeccion_id")) DOM.seccion.value = cookie("actualizaSeccion_id");
 
 		// SECCIÓN - Guarda la cookie
 		const seccion_id = DOM.seccion.value;
-		if (!v.startUp) document.cookie = "actualizaSeccion_id=" + seccion_id;
+		if (!cac.startUp) document.cookie = "actualizaSeccion_id=" + seccion_id;
 
 		// Averigua si la sección es 'Experiencias'
 		cac.tipoEncab =
@@ -97,11 +94,11 @@ window.addEventListener("load", async () => {
 	});
 	DOM.tema.addEventListener("change", () => {
 		// TEMA - Si es start-up, elige la opción de la cookie
-		if (v.startUp && cookie("actualizaTema_id")) DOM.tema.value = cookie("actualizaTema_id");
+		if (cac.startUp && cookie("actualizaTema_id")) DOM.tema.value = cookie("actualizaTema_id");
 
 		// TEMA -  Guarda la cookie
 		const tema_id = DOM.tema.value;
-		if (!v.startUp) document.cookie = "actualizaTema_id=" + tema_id;
+		if (!cac.startUp) document.cookie = "actualizaTema_id=" + tema_id;
 
 		// Averigua si el tema es 'Cartas'
 		if (cac.tipoEncab != "encabExpers")
@@ -130,12 +127,12 @@ window.addEventListener("load", async () => {
 	});
 	DOM.pestana.addEventListener("change", () => {
 		// PESTAÑA - Si es start-up, elige la opción de la cookie
-		if (v.startUp && cookie("actualizaPestana_id")) DOM.pestana.value = cookie("actualizaPestana_id");
-		delete v.startUp;
+		if (cac.startUp && cookie("actualizaPestana_id")) DOM.pestana.value = cookie("actualizaPestana_id");
+		delete cac.startUp;
 
 		// PESTAÑA -  Guarda la cookie
 		const pestana_id = DOM.pestana.value;
-		if (!v.startUp) document.cookie = "actualizaPestana_id=" + pestana_id;
+		if (!cac.startUp) document.cookie = "actualizaPestana_id=" + pestana_id;
 
 		// ENCABEZADO - Los obtiene y genera el evento 'change'
 		FN.actualizaFiltroEncabezado();
@@ -143,12 +140,8 @@ window.addEventListener("load", async () => {
 	// Eventos del filtro de encabezado
 	DOM.encabezado.addEventListener("change", async () => {
 		// ENCABEZADO - Guarda la cookie
-		if (!v.startUp) document.cookie = "actualizaEncabezado_id=" + DOM.encabezado.value;
-		// ENCABEZADO - Si es start-up, elige la opción de la cookie
-		else if (v.startUp) {
-			if (cookie("actualizaEncabezado_id")) DOM.encabezado.value = cookie("actualizaEncabezado_id");
-			delete v.startUp;
-		}
+		if (!cac.startUp) document.cookie = "actualizaEncabezado_id=" + DOM.encabezado.value;
+		delete cac.startUp;
 
 		// Actualiza el anchor de flitros
 		FN.actualizaHref();
@@ -162,7 +155,7 @@ window.addEventListener("load", async () => {
 });
 
 // Variables
-let cac; // compartirActualizarContenidos
+const cac = {startUp: true}; // compartirActualizarContenidos
 const agregaOpciones = (opciones, domSelect, campoNombre) => {
 	// Limpia las opciones del select
 	domSelect.innerHTML = "";
