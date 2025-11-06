a230187b74409dfad590522d19a359d8151f0ff7
Diego Iribarren <76135160+Diribar@users.noreply.github.com>
2025-11-06 19:29:43 -0300

    Avance del proyecto

--- mamaAntula/rutasContrs/2-ABM/ABM-controlApi.js
+++ mamaAntula/rutasContrs/2-ABM/ABM-controlApi.js
@@ -19,11 +19,14 @@ export default {
 		const condicion = {[pestana_id ? "pestana_id" : "tema_id"]: pestana_id || tema_id};
 		const {usuario} = req.session;
 
-		// Obtiene la entidad y los includes
-		const {entidad, includes} = comp.contenido.obtieneDatosDeTabla(condicion);
+		// Averigua si es carta o con índice
+		const temaActual = temasSecciones.find((n) => n.id == tema_id);
+		const esCarta = temaActual.codigo == "cartas";
+		const conIndice = !!temaActual.indices.length;
 
 		// Obtiene los encabezados
-		const encabezados = await procesos.obtieneEncabs({tema_id, entidad, condicion, includes, usuario});
+		const {entidad, includes} = comp.contenido.obtieneDatosDeTabla(condicion);
+		const encabezados = await procesos.obtieneEncabs({esCarta, conIndice, entidad, condicion, includes, usuario});
 
 		// Fin
 		return res.json(encabezados);
--- mamaAntula/rutasContrs/2-ABM/ABM-procesos.js
+++ mamaAntula/rutasContrs/2-ABM/ABM-procesos.js
@@ -1,19 +1,19 @@
 "use strict";
 
 export default {
-	obtieneEncabs: async ({entidad, condicion, includes, usuario}) => {
+	obtieneEncabs: async ({esCarta, conIndice, entidad, condicion, includes, usuario}) => {
 		// Obtiene los encabezados
 		let encabezados = esCarta
 			? baseDatos
-					.obtieneTodos("encabCartas", includes)
+					.obtieneTodos(entidad, includes)
 					.then((n) => n.sort((a, b) => new Date(a.fechaEvento) - new Date(b.fechaEvento)))
 			: baseDatos
-					.obtieneTodosPorCondicion("encabResto", condicion, includes)
+					.obtieneTodosPorCondicion(entidad, condicion, includes)
 					.then((n) => n.sort((a, b) => new Date(b.fechaEvento) - new Date(a.fechaEvento)));
 		encabezados = await encabezados;
 
 		// Si es sin indice y no existe un registro, lo crea
-		if (!esIndice && !encabezados.length) {
+		if (!conIndice && !encabezados.length) {
 			const creadoPor_id = usuario.id;
 			const statusRegistro_id = aprobado_id;
 			const datos = {...condicion, creadoPor_id, statusRegistro_id};
@@ -21,21 +21,12 @@ export default {
 			encabezado.ediciones = [];
 			encabezados = [encabezado];
 		}
-
-		// Fin
-		return encabezados;
-	},
-	consolidado: async function ({tema_id, entidad, condicion, includes, usuario}) {
-		// Obtiene los encabezados con todas sus ediciones
-		let encabezados = await this.conTodasLasEdiciones({entidad, condicion, includes, usuario});
-
 		// Les agrega los títulos
-		encabezados = comp.contenido.tituloCons[entidad](encabezados);
+		else encabezados = comp.contenido.tituloCons[esCarta ? entidad : "encabConIndice"](encabezados);
 
 		// Fin
 		return encabezados;
 	},
-
 	eliminaDepends: async (entidad, id) => {
 		// Obtiene los contenidos y los elimina
 		const campo_id = comp.contenido.obtieneCampo_id(entidad);
