9f37e6dcb6a1b0f94d9c2718ec9ad704519516d7
Diego Iribarren <76135160+Diribar@users.noreply.github.com>
2025-11-12 10:50:55 -0300

    Avance del proyecto

--- mamaAntula/publico/javascript/2-ABM-Actualizar/ABM-3ContActual.js
+++ mamaAntula/publico/javascript/2-ABM-Actualizar/ABM-3ContActual.js
@@ -10,41 +10,62 @@ window.addEventListener("load", async () => {
 		sectorContenido: document.querySelector("#sectorContActual"),
 		iconos: document.querySelector("#sectorContActual .iconos"),
 	};
-	const rutas = {
-		// Lectura
-		obtieneContenidos: "/contenido/api/abm-obtiene-contenidos/?",
-
-		// Impactos en BD
-		guardaContenido: "/contenido/api/abm-guarda-contenido",
-		bajaContenido: "/contenido/api/abm-baja-contenido",
-		subeContenido: "/contenido/api/abm-sube-contenido",
-		eliminaContenido: "/contenido/api/abm-elimina-contenido",
+	const v = {
+		campos_id: {encabCartas: "carta_id", encabResto: "encab_id"},
+		cruds: ["guarda", "baja", "sube", "elimina"],
 	};
-	const v = {campos_id: {encabCartas: "carta_id", encabResto: "encab_id"}};
+	const rutasContenido = {obtiene: "/contenido/api/abm-obtiene-contenidos/?"};
+	for (const crud of v.cruds) rutasContenido[crud] = "/contenido/api/abm-" + crud + "-contenido";
 
 	// Funciones
-	const creaContenidoIconos = {
-		consolidado: function () {
-			// Variables
-			v.inicial_id = v.contenidos[0]?.id;
-			v.final_id = v.contenidos.at(-1)?.id;
+	const creaContenidoIconos = () => {
+		// Variables
+		v.inicial_id = v.contenidos[0]?.id;
+		v.final_id = v.contenidos.at(-1)?.id;
 
-			// Agrega los contenidos
-			for (const contenido of v.contenidos) this.agregaBloque(contenido);
+		// Agrega los contenidos
+		for (const contenido of v.contenidos) FN.agregaBloque(contenido);
 
+		// Genera los eventos de los íconos
+		eventosIconos();
+
+		// Fin
+		return;
+	};
+	const eventosIconos = () => {
+		// Rutina por evento
+		for (const crud of v.cruds) {
 			// Obtiene los íconos
-			this.iconosEventos();
+			const domIconos = document.querySelectorAll("#sectorContActual .iconos ." + crud);
 
-			// Fin
-			return;
-		},
+			// Les asigna el evento a c/u
+			for (const domIcono of domIconos) {
+				domIcono.addEventListener("click", async () => {
+					// Variables
+					const id = domIcono.parentNode.dataset.id;
+
+					// Crud del contenido
+					await fetch(rutasContenido[crud], putJson({id})).then((n) => n.json());
+
+					// Actualiza el DOM
+					DOM.filtroEncab.dispatchEvent(new Event("change"));
+
+					// Fin
+					return;
+				});
+			}
+			eventosClick["icono" + evento](domIcono);
+		}
+	};
+	const FN = {
+		// Crea contenido
 		agregaBloque: function (contenido) {
 			// Crea el DOM contenedor
 			const domBloqueLectura = document.createElement("div");
 			domBloqueLectura.classList.add("bloque", "sector");
 
 			// Crea el DOM contenido
-			this.creaElContenido.consolidado(contenido); // puede ser: div (para texto, texto-imagen, carrusel), img, video
+			this.creaElContenido(contenido); // puede ser: div (para texto, texto-imagen, carrusel), img, video
 			v.domContenido.classList.add("contenido");
 			domBloqueLectura.appendChild(v.domContenido);
 
@@ -53,10 +74,11 @@ window.addEventListener("load", async () => {
 			domIconos.dataset.id = contenido.id;
 			domIconos.dataset.statusRegistro_id = contenido.statusRegistro_id;
 
-			// Si corresponde, oculta los íconos de subir y bajar
+			// Si corresponde, elimina los íconos de subir y/o bajar
 			if (v.inicial_id == contenido.id || contenido.statusRegistro_id != 1) domIconos.querySelector(".iconoSubir").remove();
-			//classList.add("ocultar");
 			if (v.final_id == contenido.id || contenido.statusRegistro_id != 1) domIconos.querySelector(".iconoBajar").remove();
+
+			// Agrega el DOM de los íconos
 			domBloqueLectura.appendChild(domIconos);
 
 			// Agrega el DOM contenedor al DOM sector
@@ -65,185 +87,119 @@ window.addEventListener("load", async () => {
 			// Fin
 			return;
 		},
-		iconosEventos: () => {
-			// Variables
-			const iconosEventos = [
-				// "Editar",
-				"Subir",
-				"Bajar",
-				"Eliminar",
-			];
+		creaElContenido: function (contenido) {
+			// Crea el DOM
+			v.domContenido = document.createElement("div");
 
-			// Rutina por evento
-			for (const evento of iconosEventos) {
-				// Obtiene los íconos
-				const iconosEvento = "iconos" + evento;
-				DOM[iconosEvento] = document.querySelectorAll("#sectorContActual .iconos .icono" + evento);
-
-				// Les asigna el evento a c/u
-				for (const icono of DOM[iconosEvento]) eventosClick["icono" + evento](icono);
+			if (false) null;
+			// Sólo texto
+			else if (contenido.texto && !contenido.imagen) v.domContenido = this.texto(contenido).cloneNode(true);
+			// Sólo una imagen
+			else if (contenido.imagen && !contenido.texto) v.domContenido = this.imagen(contenido).cloneNode(true);
+			// Texto e imagen
+			else if (contenido.texto && contenido.imagen) {
+				v.domContenido.appendChild(this.imagen(contenido));
+				v.domContenido.appendChild(this.texto(contenido));
+				v.domContenido.classList.add("textoImagen");
 			}
+			// Carrusel
+			else if (contenido.carrusel.length) this.carrusel(contenido);
+			// Video
+			else if (contenido.video) this.video(contenido);
+
+			// Fin
+			return;
 		},
+		texto: (contenido) => {
+			// Crea el contenedor
+			const domTexto = document.createElement("div");
+			domTexto.classList.add("texto");
+			domTexto.innerHTML = contenido.texto;
 
-		// Auxiliares
-		creaElContenido: {
-			consolidado: function (contenido) {
-				// Crea el DOM
-				v.domContenido = document.createElement("div");
-
-				if (false) null;
-				// Sólo texto
-				else if (contenido.texto && !contenido.imagen) v.domContenido = this.texto(contenido).cloneNode(true);
-				// Sólo una imagen
-				else if (contenido.imagen && !contenido.texto) v.domContenido = this.imagen(contenido).cloneNode(true);
-				// Texto e imagen
-				else if (contenido.texto && contenido.imagen) {
-					v.domContenido.appendChild(this.imagen(contenido));
-					v.domContenido.appendChild(this.texto(contenido));
-					v.domContenido.classList.add("textoImagen");
-				}
-				// Carrusel
-				else if (contenido.carrusel.length) this.carrusel(contenido);
-				// Video
-				else if (contenido.video) this.video(contenido);
-
-				// Fin
-				return;
-			},
-			texto: (contenido) => {
-				// Crea el contenedor
-				const domTexto = document.createElement("div");
-				domTexto.classList.add("texto");
-				domTexto.innerHTML = contenido.texto;
-
-				// Fin
-				return domTexto;
-			},
-			imagen: function (contenido) {
-				// Crea el contenedor
-				const contenedor = document.createElement("div");
-				contenedor.classList.add("contImagenLeyenda");
-
-				// Crea la imagen
-				const domImagen = document.createElement("img");
-				const subCarpeta = contenido.statusRegistro_id == 1 ? "2-Revisar/" : "1-Contenido/";
-				domImagen.src = "/imgsEditables/" + subCarpeta + contenido.imagen;
-				domImagen.classList.add("imagen");
-				contenedor.appendChild(domImagen);
-
-				// Crea la leyenda
-				if (contenido.leyenda) {
-					const domLeyenda = this.leyenda(contenido);
-					contenedor.appendChild(domLeyenda);
-				}
-
-				// Fin
-				return contenedor;
-			},
-			carrusel: function (contenido) {
-				// Crea el contenedor
-				const contenedor = document.createElement("div");
-				contenedor.classList.add("contImagenLeyenda");
-
-				// Crea el carrusel
-				v.carrImgs = document.createElement("div");
-				v.carrImgs.classList.add("carrImgs");
-				this.agregaImgsCrsl(contenido);
-				v.domContenido.appendChild(v.carrImgs);
-
-				// Crea la leyenda
-				if (contenido.leyenda) {
-					const domLeyenda = this.leyenda(contenido);
-					v.domContenido.appendChild(domLeyenda);
-				}
-
-				// Fin
-				return;
-			},
-			video: function (contenido) {
-				// Crea el iframe
-				const iframe = document.createElement("iframe");
-				iframe.src = "https://www.youtube.com/embed/" + contenido.video + "?autoplay=1";
-				v.domContenido.appendChild(iframe);
-
-				// Crea la leyenda
-				if (contenido.leyenda) {
-					const domLeyenda = this.leyenda(contenido);
-					v.domContenido.appendChild(domLeyenda);
-				}
-
-				// Fin
-				return;
-			},
-
-			// Auxiliares
-			agregaImgsCrsl: (contenido) => {
-				// Recorre el carrusel
-				for (const registro of contenido.carrusel) {
-					// Variables
-					const subCarpeta = registro.statusRegistro_id == 1 ? "2-Revisar/" : "1-Contenido/";
-
-					// Crea el domImagen
-					const img = document.createElement("img");
-					img.src = "/imgsEditables/" + subCarpeta + registro.imagen;
-
-					// La agrega al padre
-					v.carrImgs.appendChild(img);
-				}
-
-				// Fin
-				return;
-			},
-			leyenda: (contenido) => {
-				// Crea el contenedor
-				const domLeyenda = document.createElement("div");
-				domLeyenda.classList.add("leyenda");
-				domLeyenda.innerHTML = contenido.leyenda;
-
-				// Fin
-				return domLeyenda;
-			},
+			// Fin
+			return domTexto;
 		},
-	};
-	const eventosClick = {
-		iconoBajar: (iconoBajar) => {
-			iconoBajar.addEventListener("click", async () => {
-				// Baja el contenido y actualiza el DOM
-				const id = iconoBajar.parentNode.dataset.id;
-				await fetch(rutas.bajaContenido, putJson({id})).then((n) => n.json());
+		imagen: function (contenido) {
+			// Crea el contenedor
+			const contenedor = document.createElement("div");
+			contenedor.classList.add("contImagenLeyenda");
 
-				// Reinicia el DOM
-				DOM.filtroEncab.dispatchEvent(new Event("change"));
+			// Crea la imagen
+			const domImagen = document.createElement("img");
+			const subCarpeta = contenido.statusRegistro_id == 1 ? "2-Revisar/" : "1-Contenido/";
+			domImagen.src = "/imgsEditables/" + subCarpeta + contenido.imagen;
+			domImagen.classList.add("imagen");
+			contenedor.appendChild(domImagen);
 
-				// Fin
-				return;
-			});
+			// Crea la leyenda
+			if (contenido.leyenda) {
+				const domLeyenda = this.leyenda(contenido);
+				contenedor.appendChild(domLeyenda);
+			}
+
+			// Fin
+			return contenedor;
 		},
-		iconoSubir: (iconoSubir) => {
-			iconoSubir.addEventListener("click", async () => {
-				// Sube el contenido y actualiza el DOM
-				const id = iconoSubir.parentNode.dataset.id;
-				await fetch(rutas.subeContenido, putJson({id})).then((n) => n.json());
-				DOM.filtroEncab.dispatchEvent(new Event("change"));
+		carrusel: function (contenido) {
+			// Crea el contenedor
+			const contenedor = document.createElement("div");
+			contenedor.classList.add("contImagenLeyenda");
 
-				// Fin
-				return;
-			});
+			// Crea el carrusel
+			v.carrImgs = document.createElement("div");
+			v.carrImgs.classList.add("carrImgs");
+			this.agregaImgsCrsl(contenido);
+			v.domContenido.appendChild(v.carrImgs);
+
+			// Crea la leyenda
+			if (contenido.leyenda) {
+				const domLeyenda = this.leyenda(contenido);
+				v.domContenido.appendChild(domLeyenda);
+			}
+
+			// Fin
+			return;
 		},
-		iconoEliminar: (iconoEliminar) => {
-			iconoEliminar.addEventListener("click", async () => {
+		video: function (contenido) {
+			// Crea el iframe
+			const iframe = document.createElement("iframe");
+			iframe.src = "https://www.youtube.com/embed/" + contenido.video + "?autoplay=1";
+			v.domContenido.appendChild(iframe);
+
+			// Crea la leyenda
+			if (contenido.leyenda) {
+				const domLeyenda = this.leyenda(contenido);
+				v.domContenido.appendChild(domLeyenda);
+			}
+
+			// Fin
+			return;
+		},
+		agregaImgsCrsl: (contenido) => {
+			// Recorre el carrusel
+			for (const registro of contenido.carrusel) {
 				// Variables
-				const id = iconoEliminar.parentNode.dataset.id;
+				const subCarpeta = registro.statusRegistro_id == 1 ? "2-Revisar/" : "1-Contenido/";
 
-				// Elimina el contenido
-				await fetch(rutas.eliminaContenido, deleteJson({id})).then((n) => n.json());
+				// Crea el domImagen
+				const img = document.createElement("img");
+				img.src = "/imgsEditables/" + subCarpeta + registro.imagen;
 
-				// Actualiza el DOM
-				DOM.filtroEncab.dispatchEvent(new Event("change"));
+				// La agrega al padre
+				v.carrImgs.appendChild(img);
+			}
 
-				// Fin
-				return;
-			});
+			// Fin
+			return;
+		},
+		leyenda: (contenido) => {
+			// Crea el contenedor
+			const domLeyenda = document.createElement("div");
+			domLeyenda.classList.add("leyenda");
+			domLeyenda.innerHTML = contenido.leyenda;
+
+			// Fin
+			return domLeyenda;
 		},
 	};
 
@@ -252,7 +208,7 @@ window.addEventListener("load", async () => {
 		// Variables
 		const encabezado_id = DOM.filtroEncab.value;
 		const campo_id = v.campos_id[comp1234.entidad];
-		const ruta = rutas.obtieneContenidos + "encab_id=" + encabezado_id + "&campo_id=" + campo_id;
+		const ruta = rutasContenido.obtiene + "encab_id=" + encabezado_id + "&campo_id=" + campo_id;
 
 		// Limpia el DOM
 		DOM.sectorContenido.innerHTML = "";
@@ -264,7 +220,7 @@ window.addEventListener("load", async () => {
 		else DOM.sectorContenido.classList.remove("ocultar");
 
 		// Actualiza el DOM
-		creaContenidoIconos.consolidado();
+		creaContenidoIconos();
 
 		// Fin
 		return;
