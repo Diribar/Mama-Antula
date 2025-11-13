9c7a82014fad33328c12472764de3d9321c03f8c
Diego Iribarren <76135160+Diribar@users.noreply.github.com>
2025-11-13 06:54:34 -0300

    Avance del proyecto

--- mamaAntula/variables/constantes.js
+++ mamaAntula/variables/constantes.js
@@ -30,6 +30,10 @@ export default {
 	carpUsuarios: path.join(rutaHome, "..", "9-Imagenes", "8-Usuarios"),
 	carpProvisorio: path.join(rutaHome, "..", "9-Imagenes", "9-Provisorio"), // carpeta donde se guardan las imágenes inicialmente
 
+	// Landing page
+	LP_urlSeccion: "inicio",
+	LP_urlTema: "quien-es-mama-antula",
+
 	// Otras
 	requestsTriviales: ["WhatsApp", "Postman", "TelegramBot", "TwitterBot", "Zabbix"], // evita que se cuenten como visitas
 	continentes: ["América", "Europa", "Africa", "Asia", "Oceanía"],
