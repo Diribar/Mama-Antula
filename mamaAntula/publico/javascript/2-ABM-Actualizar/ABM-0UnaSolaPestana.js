b6e0a80bc45c8543e93abfc480cda9695541a79d
Diego Iribarren <76135160+Diribar@users.noreply.github.com>
2025-11-13 11:33:56 -0300

    Avance del proyecto

--- mamaAntula/publico/javascript/2-ABM-Actualizar/ABM-0UnaSolaPestana.js
+++ mamaAntula/publico/javascript/2-ABM-Actualizar/ABM-0UnaSolaPestana.js
@@ -1,18 +1,38 @@
 "use strict";
 
-window.addEventListener("load", () => {
-	// Broadcast that you're opening a page
-	localStorage.estaPestana = Date.now();
+// Broadcast that you're opening a page
+localStorage.estaPestana = Date.now();
 
-	// Funcion
-	const evento = (e) => {
-		// Sólo dispara el evento en las demás pestañas
-		if (e.key == "estaPestana") localStorage.otraPestana = Date.now();
+// Funcion
+const evento = async (e) => {
+	// Sólo dispara el evento en las demás pestañas
+	if (e.key == "estaPestana") localStorage.otraPestana = Date.now();
 
-		// Si otra pestaña avisa que está abierta, esta pestaña cambia su url
-		if (e.key == "otraPestana") location.href = "/";
-	};
+	// Si otra pestaña avisa que está abierta, esta pestaña cambia su url al de lectura
+	if (e.key == "otraPestana") {
+		// Obtiene los ids del url
+		const seccion_id = cookie("actualizaSeccion_id");
+		const tema_id = cookie("actualizaTema_id");
+		const pestana_id = cookie("actualizaPestana_id");
+		const encabezado_id = cookie("actualizaEncabezado_id");
 
-	// Evento - provocado por otra pestaña, mediante 'localStorage.otraPestana'
-	window.addEventListener("storage", evento, false);
-});
+		// Obtiene los datos iniciales
+		const v = await fetch("/contenido/api/abm-filtros-datos-inciales").then((n) => n.json());
+
+		// Obtiene los url
+		const urlSeccion = v.secciones.find((n) => n.id == seccion_id)?.url || "";
+		const urlTema = (urlSeccion && v.temasSecciones.find((n) => n.id == tema_id)?.url) || "";
+		const urlPestana = (urlTema && v.pestanasTemas.find((n) => n.id == pestana_id)?.url) || "";
+		const urlEncabezado = (urlTema && encabezado_id && "/?id=" + encabezado_id) || "";
+
+		// Redirige
+		const redirige = "/" + urlSeccion + (urlTema && "/") + urlTema + (urlPestana && "/") + urlPestana + urlEncabezado;
+		location.href = redirige;
+
+		// Fin
+		return;
+	}
+};
+
+// Evento - provocado por otra pestaña, mediante 'localStorage.otraPestana'
+window.addEventListener("storage", evento);
