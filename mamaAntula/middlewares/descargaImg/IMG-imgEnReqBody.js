b38363317328a138863d6f1af3d8dd505f18e156
Diego Iribarren <76135160+Diribar@users.noreply.github.com>
2025-11-21 18:24:24 -0300

    Avance del proyecto

--- mamaAntula/middlewares/descargaImg/IMG-imgEnReqBody.js
+++ mamaAntula/middlewares/descargaImg/IMG-imgEnReqBody.js
@@ -2,8 +2,8 @@
 
 export default (req, res, next) => {
 	// Le agrega el nombre de la/s imagenes a req.body
-	if (req.file) req.body.imagen = comp.gestionArchs.nombreArchDesc(req.file);
-	if (req.files) {
+	if (req.file || req.files.length == 1) req.body.imagen = Date.now() + path.extname((req.file || req.files[0]).originalname);
+	else if (req.files) {
 		// Variables
 		const contador = Date.now();
 
