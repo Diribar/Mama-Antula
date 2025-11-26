9a5139ddb9b04d4544b3c85dfefb3dcdd7af6cd0
Diego Iribarren <76135160+Diribar@users.noreply.github.com>
2025-11-26 12:43:23 -0300

    Avance del proyecto

--- mamaAntula/publico/javascript/3-Revisar/REV-AltaBaja.js
+++ mamaAntula/publico/javascript/3-Revisar/REV-AltaBaja.js
@@ -11,11 +11,21 @@ window.addEventListener("load", async () => {
 
 	// Eventos
 	DOM.aprobar.addEventListener("click", async () => {
+		// Si confirmar está inactivo, interrumpe la función
+		if (DOM.aprobar.classList.contains("inactivo")) return;
+		DOM.aprobar.classList.add("inactivo");
+
+		// Guarda la información en la BD
 		const datos = {encab_id, aprueba: true};
 		const respuesta = await fetch(ruta, putJson(datos)).then((n) => n.json());
 		location.reload();
 	});
 	DOM.rechazar.addEventListener("click", async () => {
+		// Si confirmar está inactivo, interrumpe la función
+		if (DOM.aprobar.classList.contains("inactivo")) return;
+		DOM.aprobar.classList.add("inactivo");
+
+		// Guarda la información en la BD
 		const datos = {encab_id, rechaza: true};
 		const respuesta = await fetch(ruta, putJson(datos)).then((n) => n.json());
 		location.reload();
