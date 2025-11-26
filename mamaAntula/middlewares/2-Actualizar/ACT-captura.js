37c68f6e1cd521a42174a90365b599492be904bf
Diego Iribarren <76135160+Diribar@users.noreply.github.com>
2025-11-26 16:11:28 -0300

    Avance del proyecto

--- mamaAntula/middlewares/2-Actualizar/ACT-captura.js
+++ mamaAntula/middlewares/2-Actualizar/ACT-captura.js
@@ -5,22 +5,18 @@ export default async (req, res, next) => {
 	const {actualizaTema_id: tema_id, actualizaPestana_id: pestana_id} = req.cookies;
 	const {id: capturadoPor_id} = req.session.usuario;
 	const capturadoEn = new Date();
-	const libera = {capturadoPor_id: null, capturadoEn: null};
 	const captura = {capturadoPor_id, capturadoEn};
 
 	// PESTAÑA - Actualiza las capturas
-	const pestsPosibles = pestanasTemas.filter((n) => n.tema_id == tema_id);
-	if (pestsPosibles.length) {
-		baseDatos.actualizaPorCondicion("temasSecciones", {capturadoPor_id}, libera);
-		baseDatos.actualizaPorCondicion("pestanasTemas", {capturadoPor_id, id: {[Op.ne]: pestana_id}}, libera); // se extiende la captura
-		baseDatos.actualizaPorCondicion("pestanasTemas", {id: pestana_id}, captura);
+	if (pestana_id) {
+		baseDatos.eliminaPorCondicion("capturas", {capturadoPor_id, pestana_id: {[Op.ne]: pestana_id}});
+		baseDatos.actualizaPorCondicion("capturas", {pestana_id}, captura); // se extiende la captura
 	}
 
 	// TEMA - Actualiza las capturas
 	else {
-		baseDatos.actualizaPorCondicion("pestanasTemas", {capturadoPor_id}, libera);
-		baseDatos.actualizaPorCondicion("temasSecciones", {capturadoPor_id, id: {[Op.ne]: tema_id}}, libera); // se extiende la captura
-		baseDatos.actualizaPorCondicion("temasSecciones", {id: tema_id}, captura);
+		baseDatos.eliminaPorCondicion("capturas", {capturadoPor_id, tema_id: {[Op.ne]: tema_id}});
+		baseDatos.actualizaPorCondicion("capturas", {tema_id}, captura); // se extiende la captura
 	}
 
 	// Fin
