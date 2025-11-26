2574cbb4bb0b5db78499521c904423c1d121b784
Diego Iribarren <76135160+Diribar@users.noreply.github.com>
2025-11-25 22:02:56 -0300

    Avance del proyecto

--- mamaAntula/rutasContrs/2-ABM/ABM-controlVista.js
+++ mamaAntula/rutasContrs/2-ABM/ABM-controlVista.js
@@ -1,4 +1,5 @@
 "use strict";
+import procesos from "./ABM-procesos.js";
 const temaVista = "abmContenido";
 
 export default {
@@ -16,6 +17,19 @@ export default {
 		// Variables
 		const codigoVista = "revisar";
 		const tituloPagina = iconosAgrupados.find((n) => n.codigo == codigoVista)?.nombre;
+
+		// Obtiene el encabezado a revisar
+		const encabsRevisar = await procesos.obtieneEncabsRevisar();
+		const encabRevisar =
+			(encabsRevisar.length > 1 && procesos.obtieneEncabRevisar(encabsRevisar)) ||
+			(encabsRevisar.length == 1 && encabsRevisar[0]) ||
+			{};
+		return res.send(encabRevisar);
+
+		// Obtiene el primer encabezado
+		const encabezado = encabezados[0];
+		const usuario = await baseDatos.obtienePorId("usuarios", encabezado.statusSugeridoPor_id);
+		encabezado.statusSugeridoPor = usuario;
 
 		// Fin
 		return res.render("CMP-0Estructura", {
