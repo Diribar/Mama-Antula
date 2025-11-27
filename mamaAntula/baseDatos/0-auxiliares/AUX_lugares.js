88a79dd533effcfa5557a64b8c900560730499fc
Diego Iribarren <76135160+Diribar@users.noreply.github.com>
2025-11-27 20:42:15 -0300

    Avance del proyecto

--- mamaAntula/baseDatos/0-auxiliares/AUX_lugares.js
+++ mamaAntula/baseDatos/0-auxiliares/AUX_lugares.js
@@ -2,6 +2,8 @@ export default (sequelize, dt) => {
 	const alias = "lugares";
 	const columns = {
 		nombre: {type: dt.STRING(20)},
+		cartas: {type: dt.BOOLEAN},
+		expers: {type: dt.BOOLEAN},
 	};
 	const config = {
 		tableName: "aux_lugares",
