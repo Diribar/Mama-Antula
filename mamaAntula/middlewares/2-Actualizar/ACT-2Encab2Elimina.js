14933ba4242b18820f392ef45429a7f3de5de624
Diego Iribarren <76135160+Diribar@users.noreply.github.com>
2025-11-27 13:19:01 -0300

    Avance del proyecto

--- mamaAntula/middlewares/2-Actualizar/ACT-2Encab2Elimina.js
+++ mamaAntula/middlewares/2-Actualizar/ACT-2Encab2Elimina.js
@@ -5,8 +5,12 @@ export default async (req, res, next) => {
 	// Variables
 	const {encab_id} = req.body;
 
-	// Valida
+	// Valida el encab_id
 	if (!encab_id) return res.json({error: "El encabezado no está identificado, no lo podemos eliminar"});
+
+	// Averigua si existe el encabezado
+	const encabezado = await baseDatos.obtienePorId("encabezados", encab_id);
+	if (!encabezado) return res.json({error: "No tenemos ese encabezado"});
 
 	// Fin
 	return next();
