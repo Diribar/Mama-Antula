cbbd629fb06e56b865ec4dec29a041bad44f9116
Diego Iribarren <76135160+Diribar@users.noreply.github.com>
2025-11-27 16:29:19 -0300

    Miscelaneas

--- mamaAntula/rutasContrs/3-Revisar/REV-controlVista.js
+++ mamaAntula/rutasContrs/3-Revisar/REV-controlVista.js
@@ -17,7 +17,6 @@ export default {
 
 		// Variables para la vista
 		const anchorLectura = procesos.anchorLectura(req);
-		console.log(20, edicion);
 
 		// Fin
 		return res.render("CMP-0Estructura", {
