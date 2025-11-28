0c691633ecc87b6e3181a38f3e1f38e97bed1b32
Diego Iribarren <76135160+Diribar@users.noreply.github.com>
2025-11-28 13:23:59 -0300

    Avance del proyecto

---
+++ mamaAntula/baseDatos/0-auxiliares/AUX_capacitacion.js
@@ -0,0 +1,19 @@
+export default (sequelize, dt) => {
+	const alias = "capacitacion";
+	const columns = {
+		// Referencias
+		orden: {type: dt.INTEGER},
+		nombre: {type: dt.STRING(25)},
+		rol_id: {type: dt.INTEGER},
+		codigo: {type: dt.STRING(20)},
+	};
+	const config = {
+		tableName: "aux_capacitacion",
+		timestamps: false,
+	};
+	const entidad = sequelize.define(alias, columns, config);
+	entidad.associate = (n) => {
+		entidad.belongsTo(n.roles, {as: "rol", foreignKey: "rol_id"});
+	};
+	return entidad;
+};
