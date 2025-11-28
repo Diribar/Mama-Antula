1870fd83039a37571e8c3889b4d281b8230df944
Diego Iribarren <76135160+Diribar@users.noreply.github.com>
2025-11-28 12:00:05 -0300

    Avance del proyecto

--- mamaAntula/rutasContrs/3-Revisar/REV-rutas.js
+++ mamaAntula/rutasContrs/3-Revisar/REV-rutas.js
@@ -8,8 +8,8 @@ import soloRevisor from "../../middlewares/8-Usuarios-Roles/US-Solo4Revisor.js";
 
 // Middlewares de API - valida errores
 import encabStatus from "../../middlewares/2-Revisar/REV-1Status-Encab.js";
-import edicion from "../../middlewares/2-Revisar/REV-2Edicion.js";
-import contStatus from "../../middlewares/2-Revisar/REV-3Status-Cont.js";
+import contStatus from "../../middlewares/2-Revisar/REV-2Status-Cont.js";
+import edicion from "../../middlewares/2-Revisar/REV-3Edicion.js";
 
 // Middlewares de API - captura
 
