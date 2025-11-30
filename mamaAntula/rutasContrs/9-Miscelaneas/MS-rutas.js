dd073893c4b68c2ecb75278f22d19442d6a10743
Diego Iribarren <76135160+Diribar@users.noreply.github.com>
2025-11-30 13:52:21 -0300

    Avance del proyecto

--- mamaAntula/rutasContrs/9-Miscelaneas/MS-rutas.js
+++ mamaAntula/rutasContrs/9-Miscelaneas/MS-rutas.js
@@ -3,7 +3,6 @@ import vista from "./MS-controlVista.js";
 
 // Middlewares de Vista
 import soloActualiz from "../../middlewares/8-Usuarios-Roles/US-Solo3Actualiz.js";
-import soloVisitasAct from "../../middlewares/8-Usuarios-Roles/US-Solo1VisitasActualiz.js";
 
 // 🧩 Router
 const router = express.Router();
@@ -11,7 +10,7 @@ const router = express.Router();
 // 🖥️ Vistas
 router.get("/contactanos", vista.contactanos);
 router.get("/biblioteca", soloActualiz, vista.biblioteca);
-router.get("/videos", soloVisitasAct, vista.videos);
+router.get("/videos", vista.videos);
 
 // ✅ Export
 export default router;
