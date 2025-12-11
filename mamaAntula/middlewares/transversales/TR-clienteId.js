a552707135fa89ce7ce3ea6fdfbb425d3d07a931
Diego Iribarren <76135160+Diribar@users.noreply.github.com>
2025-12-11 12:12:49 -0300

    Avance del proyecto

--- mamaAntula/middlewares/transversales/TR-clienteId.js
+++ mamaAntula/middlewares/transversales/TR-clienteId.js
@@ -12,10 +12,10 @@ export default async (req, res, next) => {
 	// Si el 'cliente_id' tiene un valor y coincide en ambas variables, interrumpe la función
 	if (usuario && cliente && usuario.cliente_id && usuario.cliente_id == cliente.cliente_id) {
 		// Si corresponde, actualiza la fecha de última navegación
-		if (usuario.fechaUltNaveg < fechaHoy) {
-			await baseDatos.actualizaPorId("usuarios", usuario.id, {fechaUltNaveg: fechaHoy});
-			req.session.usuario.fechaUltNaveg = fechaHoy;
-			req.session.cliente.fechaUltNaveg = fechaHoy;
+		if (usuario.fechaUltNaveg < fechaHoy || usuario.versionWeb != versionWeb) {
+			await baseDatos.actualizaPorId("usuarios", usuario.id, {fechaUltNaveg: fechaHoy, versionWeb});
+			req.session.usuario = {...req.session.usuario, fechaUltNaveg: fechaHoy, versionWeb};
+			req.session.cliente = {...req.session.cliente, fechaUltNaveg: fechaHoy, versionWeb};
 		}
 
 		// Actualiza locals
@@ -84,17 +84,17 @@ export default async (req, res, next) => {
 	}
 
 	// Si corresponde, actualiza la fecha de última navegación
-	if (cliente.fechaUltNaveg < fechaHoy) {
+	if (cliente.fechaUltNaveg < fechaHoy || cliente.versionWeb != versionWeb) {
 		// Actualiza el cliente
-		cliente.fechaUltNaveg = fechaHoy;
+		cliente = {...cliente, fechaUltNaveg: fechaHoy, versionWeb};
 
 		// Actualiza el usuario
 		if (usuario) {
-			baseDatos.actualizaPorId("usuarios", usuario.id, {fechaUltNaveg: fechaHoy});
-			usuario.fechaUltNaveg = fechaHoy;
+			baseDatos.actualizaPorId("usuarios", usuario.id, {fechaUltNaveg: fechaHoy, versionWeb});
+			usuario = {...usuario, fechaUltNaveg: fechaHoy, versionWeb};
 		}
 		// Actualiza la visita
-		else baseDatos.actualizaPorId("visitas", cliente.id, {fechaUltNaveg: fechaHoy});
+		else baseDatos.actualizaPorId("visitas", cliente.id, {fechaUltNaveg: fechaHoy, versionWeb});
 	}
 
 	// Actualiza usuario y cliente
