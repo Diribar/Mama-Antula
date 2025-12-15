5241d901c7c0ec7fc5d0a25604ca3cfe2b026140
Diego Iribarren <76135160+Diribar@users.noreply.github.com>
2025-12-15 18:14:25 -0300

    Avance del proyecto

--- mamaAntula/publico/javascript/0-Compartido/CMP-MuestraOculta.js
+++ mamaAntula/publico/javascript/0-Compartido/CMP-MuestraOculta.js
@@ -7,7 +7,7 @@ window.addEventListener("load", () => {
 		menus: document.querySelectorAll(".mostrarToggle"),
 
 		// Relacionado con búsqueda rápida
-		brIcono: document.querySelector("header #busquedaRapida .mostrarClick"),
+		menuMobile: document.querySelector("header #menuMobile"),
 		brMostrar: document.querySelector("header #busquedaRapida .mostrarToggle"), // es el contenedor del input
 		brInput: document.querySelector("header #busquedaRapida .mostrarToggle input"), // es el input
 	};
@@ -20,11 +20,18 @@ window.addEventListener("load", () => {
 			if (menu.contains(e.target)) return;
 
 			// Se fija si el 'click' fue en el ícono y si está activo
-			e.target == click && !click.className.includes("inactivo")
+			e.target == click && !click.classList.contains("inactivo")
 				? menu.classList.toggle("ocultar") // caso exitoso, toggle del menu
 				: menu.classList.add("ocultar"); // caso fallido, oculta el menu
 
-			if (e.target == click && click == DOM.brIcono && !DOM.brMostrar.classList.contains("ocultar")) DOM.brInput.focus();
+			// Si el click fue 'menú Mobile búsqueda rápida', muestra el input
+			if (e.target.classList.contains("brMobile")){
+				DOM.brMostrar.classList.remove("ocultar");
+				DOM.menuMobile.classList.add("ocultar");
+			}
+
+			// Pone el foco en el input de la búsqueda rápida
+			if (!DOM.brMostrar.classList.contains("ocultar")) DOM.brInput.focus();
 		})
 	);
 });
