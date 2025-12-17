9b1e8cf3370eba0253e3f4a47a62a27d6aed2ee3
Diego Iribarren <76135160+Diribar@users.noreply.github.com>
2025-12-17 12:25:06 -0300

    Avance del proyecto

--- mamaAntula/publico/javascript/1-Lectura/LT-2Anchors.js
+++ mamaAntula/publico/javascript/1-Lectura/LT-2Anchors.js
@@ -3,10 +3,12 @@
 window.addEventListener("load", async () => {
 	// Variables
 	const DOM = {
-		aumentaDisminuye: document.querySelector("#ladoIzq #aumentaDisminuye"),
+		ladoIzq: document.querySelector("#ladoIzq"),
+		muestraFiltroIndice: document.querySelector("#ladoIzq #muestraFiltroIndice"),
 		filtroIndice: document.querySelector("#ladoIzq #filtroIndice"),
 		titulos: document.querySelectorAll("#filtroIndice .titulo"),
 	};
+	document.addEventListener("click", (e) => console.log(e.target));
 
 	// Funciones
 	const ocultaMuestra = ({titulo, accion}) => {
@@ -25,25 +27,23 @@ window.addEventListener("load", async () => {
 	};
 
 	// Eventos - Muestra/oculta el lado izquierdo
-	DOM.aumentaDisminuye.addEventListener("click", () => {
+	DOM.muestraFiltroIndice.addEventListener("click", () => {
 		// Ancho de la zona de filtros
 		DOM.filtroIndice.classList.toggle("aumentaX");
 		const esAumentaX = DOM.filtroIndice.classList.contains("aumentaX");
 		DOM.filtroIndice.classList[esAumentaX ? "remove" : "add"]("disminuyeX");
 
 		// Flecha
-		DOM.aumentaDisminuye.classList.toggle("flechaIzq");
-		const esFlechaIzq = DOM.aumentaDisminuye.classList.contains("flechaIzq");
-		DOM.aumentaDisminuye.classList[esFlechaIzq ? "remove" : "add"]("flechaDer");
+		DOM.muestraFiltroIndice.classList.toggle("ocultar");
 	});
 
 	// Eventos - Gira el ícono y muestra/oculta los encabezados
 	for (const titulo of DOM.titulos)
 		titulo.addEventListener("click", () => {
 			// Alterna entre girar o no el ícono
-			const mostrar = titulo.querySelector(".mostrar");
-			if (!mostrar) return;
-			mostrar.classList.toggle("girar");
+			const muestraFiltrosAnchors = titulo.querySelector(".muestraFiltrosAnchors");
+			if (!muestraFiltrosAnchors) return;
+			muestraFiltrosAnchors.classList.toggle("girar");
 
 			// Alterna entre mostrar y ocultar los encabezados
 			ocultaMuestra({titulo, accion: "toggle"});
@@ -60,9 +60,9 @@ window.addEventListener("load", async () => {
 		// Endereza todos los íconos y oculta los encabezados
 		for (const titulo of DOM.titulos) {
 			// Endereza el ícono
-			const mostrar = titulo.querySelector(".mostrar");
-			if (!mostrar) continue;
-			mostrar.classList.remove("girar");
+			const muestraFiltrosAnchors = titulo.querySelector(".muestraFiltrosAnchors");
+			if (!muestraFiltrosAnchors) continue;
+			muestraFiltrosAnchors.classList.remove("girar");
 
 			// Oculta los encabezados/filtros
 			ocultaMuestra({titulo, accion: "add"});
