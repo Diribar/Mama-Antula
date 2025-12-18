9c3ff58f1a67ee884295ac76318bc042ea52cef4
Diego Iribarren <76135160+Diribar@users.noreply.github.com>
2025-12-18 15:35:09 -0300

    Avance del proyecto

--- mamaAntula/middlewares/1-Lectura/LT-libera.js
+++ mamaAntula/middlewares/1-Lectura/LT-libera.js
@@ -5,21 +5,10 @@ export default (req, res, next) => {
 	const {actTema_id: tema_id, actPestana_id: pestana_id} = req.cookies;
 	const {usuario} = req.session;
 
-	// Si no hay usuario, interrumpe la función
-	if (!usuario) return next();
-
-	// PESTAÑA - Si está capturado por el usuario, lo libera
-	const libera = {capturadoPor_id: null, capturadoEn: null};
-	if (pestana_id)
-		baseDatos
-			.obtienePorId("pestanasTemas", pestana_id)
-			.then((n) => n && n.capturadoPor_id == usuario.id && baseDatos.actualizaPorId("pestanasTemas", pestana_id, libera));
-	// TEMA - Si está capturado por el usuario, lo libera
-	else if (tema_id) {
-		baseDatos
-			.obtienePorId("temasSecciones", tema_id)
-			.then((n) => n && n.capturadoPor_id == usuario.id && baseDatos.actualizaPorId("temasSecciones", tema_id, libera));
-	}
+	// Elimina la captura del usuario
+	const campo = tema_id ? "tema_id" : "pestana_id";
+	const campo_id = tema_id || pestana_id;
+	if (usuario) baseDatos.eliminaPorCondicion("capturas", {[campo]: campo_id, capturadoPor_id: usuario.id});
 
 	// Fin
 	return next();
