eb60dce85e6cabce350aca15f48dffd6256202e1
Diego Iribarren <76135160+Diribar@users.noreply.github.com>
2025-12-24 16:55:56 -0300

    Avance del proyecto

--- mamaAntula/funciones/compartidas.js
+++ mamaAntula/funciones/compartidas.js
@@ -258,17 +258,13 @@ export default {
 	},
 	omitirMiddlewsTransv: (req) => {
 		// Si es un url irrelevante
-		if (["/favicon.ico"].includes(req.originalUrl)) return true;
 		if (req.originalUrl.includes("/api/")) return true;
-		if (req.originalUrl == "/.well-known/appspecific/com.chrome.devtools.json") return true;
-		if (req.originalUrl.startsWith("/xmlrpc.php")) return true;
-		if (["/imgsEstables/", "/imgsEditables/", "/9-Imagenes/"].some((n) => req.originalUrl.startsWith(n))) return true;
-		if (["/sitemap.xml.gz", "/robots.txt", "/9-Imagenes/"].some((n) => req.originalUrl.startsWith(n))) return true;
+		if (req.originalUrl.includes(".")) return true;
 
-		// Si se desconoce el origen
+		// Se desconoce el origen
 		if (!req.headers["user-agent"]) return true;
 
-		// Si es una aplicación conocida que no es de navegación, pero que muestra datos del url visitado
+		// Es una aplicación conocida que no es de navegación, pero que muestra datos del url visitado
 		if (requestsTriviales.some((n) => req.headers["user-agent"].startsWith(n))) return true;
 
 		// Fin
