b3bdac1ddc33542f4822f9f72bbe5b1038c4e2f4
Diego Iribarren <76135160+Diribar@users.noreply.github.com>
2026-01-05 16:41:57 -0300

    Avance del proyecto

--- mamaAntula/rutasContrs/9-Miscelaneas/MS-controlApi.js
+++ mamaAntula/rutasContrs/9-Miscelaneas/MS-controlApi.js
@@ -7,40 +7,25 @@ export default {
 		// Variables
 		const {nombre: remitenteNombre, email: remitenteMail, institucion, mensaje} = req.body;
 		const nombre = "Familia Mama Antula";
-		let asunto = "Mensaje enviado desde el formulario de contacto";
-
-		// Cuerpo del mail
-		let comentario =
-			"Este es un mensaje enviado desde el formulario de contacto de Mama Antula:<br><br>" +
-			("<b>Nombre:</b> " + remitenteNombre + "<br>") +
-			("<b>Mail:</b> " + remitenteMail + "<br>") +
-			(institucion ? "<b>Institución:</b> " + institucion + "<br>" : "") +
-			("<b>Mensaje:</b><br>" + mensaje.replace(/\n/g, "<br>") + "<br><br>") +
-			("Por favor, respondé directamente al remitente a su mail: <b><em>" + remitenteMail + "</em></b>.");
+		let asunto, comentario, mailEnviado;
 
 		// Envía el mail a los miembros de la Familia Mama Antula
 		const emailsRevisores = await comp.emailsRevisores();
-		let mailEnviado;
+		asunto = "Mensaje enviado desde el formulario de contacto";
+		comentario = procesos.comentarios.revisores({remitenteNombre, remitenteMail, institucion, mensaje});
 		for (const email of emailsRevisores)
 			mailEnviado = (await comp.enviaMail({nombre, email, asunto, comentario})) || mailEnviado;
-
-		// Genera el mensaje de respuesta
-		const error = !mailEnviado ? "No hemos podido enviar el mail" : "";
 
 		// Envía copia al remitente
 		if (mailEnviado) {
 			const email = remitenteMail;
 			asunto = "Mensaje enviado desde el formulario de contacto";
-			comentario =
-				"Hola " +
-				remitenteNombre +
-				",<br><br>" +
-				"Gracias por comunicarte con nosotros. Te confirmamos que tu mensaje fue enviado al equipo.<br><br>" +
-				("Tu mensaje fue:<br><em>" + mensaje.replace(/\n/g, "<br>") + "</em><br><br>") +
-				"Saludos cordiales,<br>" +
-				"La Familia Mama Antula.";
+			comentario = procesos.comentarios.remitente({remitenteNombre, mensaje});
 			await comp.enviaMail({nombre, email, asunto, comentario});
 		}
+
+		// Genera el mensaje de respuesta
+		const error = !mailEnviado ? "No hemos podido enviar el mail" : "";
 
 		// Fin
 		return res.json({error});
