36a6522d00f4000b0c42e0e2c43a72a31faec4c2
Diego Iribarren <76135160+Diribar@users.noreply.github.com>
2026-03-12 17:11:43 -0300

    Solución de errores

--- mamaAntula/middlewares/9-Miscelaneas/MS-RutaVisitada.js
+++ mamaAntula/middlewares/9-Miscelaneas/MS-RutaVisitada.js
@@ -3,10 +3,10 @@
 export default async (req, res, next) => {
 	// Variables
 	const originalUrl = req.originalUrl.slice(0, 100); // para analizar el url
-	const {cliente_id} = req.session.cliente;
+	const {cliente_id} = req.session.cliente || {};
 
 	// Actualiza la tabla navegacs - hace falta el 'await' para que pueda ser eliminada si corresponde
-	await baseDatos.agregaRegistroIdCorrel("navegacs", {cliente_id, originalUrl});
+	if (cliente_id) await baseDatos.agregaRegistroIdCorrel("navegacs", {cliente_id, originalUrl});
 
 	// Fin
 	return next();
